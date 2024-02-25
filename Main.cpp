cb520757f5b95bee400dc5f49562e751ed423dc0
AkazuEve <134776829+AkazuEve@users.noreply.github.com>
2024-02-25 09:12:33 +0100

    ImGui and OpenGL DSA

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,9 @@
 #include "Shader.hpp"
 #include "Camera.hpp"
 
-unsigned int RENDER_WIDTH = 1920;
-unsigned int RENDER_HEIGHT = 1080;
+
+unsigned int RENDER_WIDTH = 1280;
+unsigned int RENDER_HEIGHT = 720;
 
 std::vector<WindowHint> hints = { WindowHint(GLFW_CONTEXT_VERSION_MAJOR,	4),
 								  WindowHint(GLFW_CONTEXT_VERSION_MINOR,	6),
@@ -11,9 +12,19 @@ std::vector<WindowHint> hints = { WindowHint(GLFW_CONTEXT_VERSION_MAJOR,	4),
 
 GLfloat vertices[] = {
 	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
 };
+
+GLuint indices[] = {
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Upper triangle
+	5, 4, 1 // Lower right triangle
+};
+
 
 int main() {
 	Window::Init(RENDER_WIDTH, RENDER_HEIGHT, "Game Window", hints);
@@ -30,19 +41,24 @@ int main() {
 
 	glUniformMatrix4fv(glGetUniformLocation(shader.GetID(), "model"), 1, GL_FALSE, glm::value_ptr(model));
 
-	GLuint VertexArrayObject, VertexBufferObject;
+	GLuint VertexArrayObject, VertexBufferObject, IndexBufferObject;
 
-	glGenVertexArrays(1, &VertexArrayObject);
-	glGenBuffers(1, &VertexBufferObject);
+	glCreateVertexArrays(1, &VertexArrayObject);
+	glCreateBuffers(1, &VertexBufferObject);
+	glCreateBuffers(1, &IndexBufferObject);
+
+	glNamedBufferData(VertexBufferObject, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glNamedBufferData(IndexBufferObject, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glEnableVertexArrayAttrib(VertexArrayObject, 0);
+	glVertexArrayAttribBinding(VertexArrayObject, 0, 0);
+	glVertexArrayAttribFormat(VertexArrayObject, 0, 3, GL_FLOAT, GL_FALSE, 0);
+
+	glVertexArrayVertexBuffer(VertexArrayObject, 0, VertexBufferObject, 0, 3 * sizeof(GLfloat));
+	glVertexArrayElementBuffer(VertexArrayObject, IndexBufferObject);
+	#pragma endregion
 
 	glBindVertexArray(VertexArrayObject);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
-	glEnableVertexAttribArray(0);
-	#pragma endregion
 
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
@@ -51,8 +67,10 @@ int main() {
 
 		camera.SendMatrix(shader);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
 	}
+
+	Window::Terminate();
 
 	return 0;
 }
